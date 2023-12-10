d30ee49669b3b9ef30cdb913e91677759e6a0847
Donia Ahmed <112786733+DoniaAhmed20@users.noreply.github.com>
2023-12-10 11:16:48 +0200

    Update update.php
    Pppp

---
+++ views/customer/update.php
@@ -0,0 +1,37 @@
+<?php
+// Assuming you have a database connection established
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Replace 'your_user_id' with the actual user ID
+$user_id = 1; // Replace with the actual user ID
+
+// Fetch posts for the specified user
+$sql = "SELECT * FROM posts WHERE user_id = $user_id";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Display posts
+    while ($row = $result->fetch_assoc()) {
+        echo "Post ID: " . $row["post_id"] . "<br>";
+        echo "Title: " . $row["title"] . "<br>";
+        echo "Content: " . $row["content"] . "<br>";
+        // Add other post details as needed
+        echo "<hr>";
+    }
+} else {
+    echo "No posts found for the user.";
+}
+
+// Close the connection
+$conn->close();
+?>
